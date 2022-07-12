3ad3476108a11b49aba7808431b42be6a6c3c9d1
Zehra Sena Akgül <74866817+zehrasenakgul@users.noreply.github.com>
2022-07-12 19:15:27 +0300

    User::create instead of DB::table

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class AdminUserSeeder extends Seeder
 {
@@ -14,7 +15,7 @@ class AdminUserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        User::create([
             "name" => "admin",
             "email" => "admin@login.com",
             "password" => bcrypt("admin")
