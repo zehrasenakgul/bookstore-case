03efdf1c8c914b3ad00c8cd5a129eb07d5f93dfd
zehrasenakgul <74866817+zehrasenakgul@users.noreply.github.com>
2022-08-17 01:02:43 +0300

    heroku ssl

--- resources/views/admin/login/index.blade.php
+++ resources/views/admin/login/index.blade.php
@@ -8,6 +8,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <title>Yönetim Paneli | Admin Girişi </title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
     <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
