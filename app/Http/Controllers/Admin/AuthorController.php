dceeed1f0ac646bf8324b7dc6eeb40c9fc9a6ef2
zehrasenakgul <74866817+zehrasenakgul@users.noreply.github.com>
2022-08-17 01:11:51 +0300

    heroku route update

--- app/Http/Controllers/Admin/AuthorController.php
+++ app/Http/Controllers/Admin/AuthorController.php
@@ -10,56 +10,56 @@ use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
 
 class AuthorController extends Controller
-{
+ {
     public function create()
-    {
-        return view('admin.authors.add');
+ {
+        return view( 'admin.authors.add' );
     }
 
     public function index()
-    {
+ {
         $authors = Author::all();
 
-        return view('admin.authors.index', compact('authors'));
+        return view( 'admin.authors.index', compact( 'authors' ) );
     }
 
-    public function edit(Author $author)
-    {
-        return view('admin.authors.update', compact('author'));
+    public function edit( Author $author )
+ {
+        return view( 'admin.authors.update', compact( 'author' ) );
     }
 
-    public function store(CreateAuthorRequest $request)
-    {
+    public function store( CreateAuthorRequest $request )
+ {
         $author = new Author();
-        $author->name = $request->input('name');
-        $author->status = $request->input('status');
-        $str = Str::slug($request->input('name'), '-');
+        $author->name = $request->input( 'name' );
+        $author->status = $request->input( 'status' );
+        $str = Str::slug( $request->input( 'name' ), '-' );
         $author->slug = $str;
         $author->save();
-        Session::flash('alertSuccessMessage', 'Yazar Kaydı Başarılı!');
+        Session::flash( 'alertSuccessMessage', 'Yazar Kaydı Başarılı!' );
 
-        return redirect()->route('admin.authors.index');
+        return redirect()->route( 'admin.authors.index' );
     }
 
-    public function update(Request $request, Author $author)
-    {
-        $str = Str::slug($request->input('name'), '-');
-        $author->update([
-            'name' => $request->input('name'),
-            'status' => $request->input('status'),
+    public function update( Request $request, Author $author )
+ {
+        $str = Str::slug( $request->input( 'name' ), '-' );
+        $author->update( [
+            'name' => $request->input( 'name' ),
+            'status' => $request->input( 'status' ),
             'slug' => $str,
-        ]);
+        ] );
 
-        Session::flash('alertSuccessMessage', 'Yazar Güncelleme Başarılı!');
+        Session::flash( 'alertSuccessMessage', 'Yazar Güncelleme Başarılı!' );
 
-        return redirect()->route('authors.index');
+        return redirect()->route( 'admin.authors.index' );
     }
 
-    public function destroy(Author $author)
-    {
+    public function destroy( Author $author )
+ {
         $author->delete();
-        Session::flash('alertSuccessMessage', 'Yazar Silme Başarılı!');
+        Session::flash( 'alertSuccessMessage', 'Yazar Silme Başarılı!' );
 
-        return redirect()->route('admin.authors.index');
+        return redirect()->route( 'admin.authors.index' );
     }
 }
--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -14,81 +14,83 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class BookController extends Controller
-{
+ {
     public function create()
-    {
-        $authors = Author::where('status', '1')->get();
+ {
+        $authors = Author::where( 'status', '1' )->get();
 
-        return view('admin.books.add', compact('authors'));
+        return view( 'admin.books.add', compact( 'authors' ) );
     }
 
-    public function edit(Book $book)
-    {
-        $authors = Author::where('status', '1')->get();
+    public function edit( Book $book )
+ {
+        $authors = Author::where( 'status', '1' )->get();
 
-        return view('admin.books.update', compact('authors', 'book'));
+        return view( 'admin.books.update', compact( 'authors', 'book' ) );
     }
 
     public function index()
-    {
+ {
         $books = Book::all();
 
-        return view('admin.books.index', compact('books'));
+        return view( 'admin.books.index', compact( 'books' ) );
     }
 
     //FormRequest
-    public function store(CreateBookRequest $request)
-    {
+
+    public function store( CreateBookRequest $request )
+ {
         $book = new Book();
         $filePath = noImagePath::PATH;
-        if ($request->hasFile('image')) {
-            $filePath = Storage::disk('storage')->put('books', $request->file('image'), 'public');
+        if ( $request->hasFile( 'image' ) ) {
+            $filePath = Storage::disk( 'storage' )->put( 'books', $request->file( 'image' ), 'public' );
         }
-        $book->name = $request->input('name');
-        $book->book_no = $request->input('book_no');
-        $book->author_id = $request->input('author_id');
-        $book->status = $request->input('status');
+        $book->name = $request->input( 'name' );
+        $book->book_no = $request->input( 'book_no' );
+        $book->author_id = $request->input( 'author_id' );
+        $book->status = $request->input( 'status' );
         $book->image = $filePath;
-        $str = Str::slug($request->name, '-');
+        $str = Str::slug( $request->name, '-' );
         $book->slug = $str;
         $book->save();
-        Session::flash('alertSuccessMessage', 'Kitap Kaydı Başarılı!');
-        return redirect()->route('admin.books.index');
+        Session::flash( 'alertSuccessMessage', 'Kitap Kaydı Başarılı!' );
+        return redirect()->route( 'admin.books.index' );
     }
 
     //FormRequest
-    public function update(Request $request, Book $book)
-    {
+
+    public function update( Request $request, Book $book )
+ {
         $filePath = $book->image;
-        if ($request->hasFile('image')) {
-            if ($book->image != noImagePath::PATH) {
-                Storage::disk('storage')->delete($book->image);
+        if ( $request->hasFile( 'image' ) ) {
+            if ( $book->image != noImagePath::PATH ) {
+                Storage::disk( 'storage' )->delete( $book->image );
             }
-            $filePath = Storage::disk('storage')->put('books', $request->file('image'), 'public');
+            $filePath = Storage::disk( 'storage' )->put( 'books', $request->file( 'image' ), 'public' );
         }
-        $str = Str::slug($request->name, '-');
-        $book->name = $request->input('name');
-        $book->book_no = $request->input('book_no');
-        $book->author_id = $request->input('author_id');
-        $book->status = $request->input('status');
+        $str = Str::slug( $request->name, '-' );
+        $book->name = $request->input( 'name' );
+        $book->book_no = $request->input( 'book_no' );
+        $book->author_id = $request->input( 'author_id' );
+        $book->status = $request->input( 'status' );
         $book->image = $filePath;
-        $str = Str::slug($request->name, '-');
+        $str = Str::slug( $request->name, '-' );
         $book->slug = $str;
         $book->save();
-        Session::flash('alertSuccessMessage', 'Kitap Güncelleme Başarılı!');
-        // return redirect()->action([BookController::class, 'index']);
-        return redirect()->route('admin.books.index');
+        Session::flash( 'alertSuccessMessage', 'Kitap Güncelleme Başarılı!' );
+        // return redirect()->action( [ BookController::class, 'index' ] );
+        return redirect()->route( 'admin.books.index' );
     }
 
-    public function destroy(Book $book)
-    {
+    public function destroy( Book $book )
+ {
         //soft delete yaptığımız için bu işleme gerek yok görseli silmiyor ;
-        // if ($book->image != noImagePath::PATH) {
-        //     Storage::disk('storage')->delete($book->image);
+        // if ( $book->image != noImagePath::PATH ) {
+        //     Storage::disk( 'storage' )->delete( $book->image );
         // }
         $book->delete();
-        Session::flash('alertSuccessMessage', 'Kitap Silme Başarılı!');
+        Session::flash( 'alertSuccessMessage', 'Kitap Silme Başarılı!' );
 
-        return redirect()->route('addmin.books.index');
+        return redirect()->route( 'admin.books.index' );
     }
 }
